ce7bea46af2dfd291353a0f4a5b32f71d4312d4d
Ron Almog <ron8132@gmail.com>
2019-10-28 08:08:43 -0400

    use navigation in main component

--- src/app/components/main.jsx
+++ src/app/components/main.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import { Route, Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from '../store';
 import { ConnectedDashboard } from './dashboard';
+import { history } from '../store/history';
+import { ConnectedNavigation } from './navigation';
 
-export const Main = ()=>(
-    <Provider store={store}>
-        <div>
-           <ConnectedDashboard></ConnectedDashboard>
-        </div>
-    </Provider>
+export const Main = () => (
+    <Router history={history}>
+        <Provider store={store}>
+            <div>
+                <ConnectedNavigation/>
+                {/* <ConnectedDashboard></ConnectedDashboard> */}
+                <Route exact path="/dashboard" render={()=>(<ConnectedDashboard/>)} />
+            </div>
+        </Provider>
+    </Router>
 )
