0f7f6eb0d60e5c12d6f9dcedff08cb2318a29803
Ron Almog <ron8132@gmail.com>
2019-10-28 08:09:32 -0400

    add redux, store, reducers, saga

--- src/app/components/taskList.jsx
+++ src/app/components/taskList.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { requestTaskCreation } from '../store/mutations';
 
-export const TaskList = ({ tasks, name }) => (
+export const TaskList = ({ tasks, name, id, createNewTask }) => (
     <div>
         <h3>{name}</h3>
         <div>
-            {tasks.map(task => (<div>{task.name}</div>))}
+            {tasks.map(task => (<div key={task.id}>{task.name} - {task.id}</div>))}
         </div>
+        <button onClick={()=>createNewTask(id)}>Add New</button>
     </div>
 );
 
@@ -19,4 +21,13 @@ const mappStateToProps = (state, ownProps) => {
     }
 };
 
-export const ConnectedTaskList = connect(mappStateToProps)(TaskList);
+const mapDispatchToProps = (dispatch, ownProps)=>{
+    return {
+        createNewTask(id) {
+            console.log('Creating new task...', id);
+            dispatch(requestTaskCreation(id));
+        }
+    }
+}
+
+export const ConnectedTaskList = connect(mappStateToProps, mapDispatchToProps)(TaskList);
